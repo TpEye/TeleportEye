4da692a2527dd6e7d8d4f46cf4afe7c9ce450d5c
nik <newintegral@gmail.com>
2016-02-28 19:37:39 +0200

    1. Changing font color. 2. Add negative margin for social buttons to fit design template. 3. Change main logo image size and margin. 4. Change font size for "The future..." label.

--- app/src/main/java/com/teleporteye/MainActivity.java
+++ app/src/main/java/com/teleporteye/MainActivity.java
@@ -2,12 +2,19 @@ package com.teleporteye;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide title bar.
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
     }
 }
